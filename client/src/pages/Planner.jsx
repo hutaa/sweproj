6c26753c4cb7ae7e252fae8b46089126a3489771
akusagisei <145866870+akusagisei@users.noreply.github.com>
2025-10-24 16:00:30 -0400

    Added a prototype for enforcing prerequisites.
    Revision Summary:
    • Migrated to dnd-kit for handling drag and drop interactions.
    • Fixed functionality for enforcing prerequisites.

--- client/src/pages/Planner.jsx
+++ client/src/pages/Planner.jsx
@@ -1,31 +1,65 @@
 import React, {useState, useEffect} from "react";
+import {DndContext, useDraggable, useDroppable} from '@dnd-kit/core';
+
 import "../styles/Planner.css"
 
-function CourseNode({course_code, course_desc})
+function Draggable(props) {
+    const {attributes, listeners, setNodeRef, transform} = useDraggable({
+      id: props.id,
+    });
+
+    const style = transform ? {
+      transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+    } : undefined;
+  
+    return (
+      <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
+        {props.children}
+      </button>
+    );
+}
+
+function Droppable(props) {
+    const {isOver, setNodeRef} = useDroppable({
+        id: props.id,
+    });
+
+    const style = {
+        color: isOver ? 'green' : undefined,
+    };
+
+    return (
+        <div ref={setNodeRef} style={style}>
+            {props.children}
+        </div>
+    );
+}
+
+function CourseNode({course_id, course_code, course_desc})
 {
     return (
-        <div className="course-node-container">
+        <Draggable className="course-node-container" id={course_id}>
             <div className="body-text">
                 {course_code}
             </div>
             <div className="body-text">
                 {course_desc}
             </div>
-        </div>
+        </Draggable>
     );
 }
 
 function SemesterGroup({semester_name, children})
 {
     return (
-        <div>
+        <Droppable id={semester_name}>
             <div className="body-text">
                 {semester_name}
             </div>
             <div className="course-node-group-container col-container" style={{gap: "12px"}}>
                 {children}
             </div>
-        </div>
+        </Droppable>
     )
 }
 
@@ -33,37 +67,14 @@ function Planner()
 {
     const semesters = ["FA25", "SP26", "FA26"];
     const [courses, SetCourses] = useState([
-        {id: 0, semester: "FA25", course_code: "TEST 123", course_desc: "Introduction to Testing 1", prerequisites: []},
-        {id: 1, semester: "SP26", course_code: "TEST 234", course_desc: "Introduction to Testing 2", prerequisites: ["TEST 123"]},
-        {id: 2, semester: "FA26", course_code: "TEST 345", course_desc: "Introduction to Testing 3", prerequisites: ["TEST 123", "TEST 234"]},
+        {id: 0, semester: null, course_code: "TEST 123", course_desc: "Introduction to Testing 1", prerequisites: []},
+        {id: 1, semester: null, course_code: "TEST 234", course_desc: "Introduction to Testing 2", prerequisites: ["TEST 123"]},
+        {id: 2, semester: null, course_code: "TEST 345", course_desc: "Introduction to Testing 3", prerequisites: ["TEST 123", "TEST 234"]},
     ]);
 
     const [dragging, SetDragging] = useState();
 
     const [valid_semesters, SetValidSemesters] = useState(); //Used for keeping track of valid placements for the current course being dragged.
-
-    const handleDragStart = (e, id) =>
-    {
-        SetDragging(e.target);
-
-        var temp = [];
-        for (let i = 0; i < semesters.length; i++)
-        {
-            if (CheckPrerequisitesMet(id, SemesterToInt(semesters[i])) === true)
-            {
-                temp.push(semesters[i]);
-            }
-        }
-        SetValidSemesters(temp);
-    };
-
-    const onDragEnter = (e, semester) =>
-    {
-        if (valid_semesters.includes(semester) === true)
-        {
-            SetCourses([...courses, (courses[dragging.id].semester = semester)]);
-        }
-    };
 
     /*
 
@@ -89,79 +100,81 @@ function Planner()
 
     function SemesterToInt(semester)
     {
-        var temp = semester;
-        temp = temp.substring(0, 2);
+        if (semester)
+        {
+            var temp = String(semester);
+            temp = temp.substring(0, 2);
 
-        // Compute last digit of the number.
-        if (temp === "WI")
-        {
-            temp = 1;
-        }
-        else if (temp === "SP")
-        {
-            temp = 2;
-        }
-        else if (temp === "SU")
-        {
-            temp = 3;
-        }
-        else if (temp === "FA")
-        {
-            temp = 4;
-        }
-        
-        // Checks if a valid substring was read.
-        if (Number.isInteger(temp) === true)
-        {
-            return parseInt(semester.substring(2, 6)) * 10 + temp;
+            // Compute last digit of the number.
+            if (temp === "WI")
+            {
+                temp = 1;
+            }
+            else if (temp === "SP")
+            {
+                temp = 2;
+            }
+            else if (temp === "SU")
+            {
+                temp = 3;
+            }
+            else if (temp === "FA")
+            {
+                temp = 4;
+            }
+            
+            // Checks if a valid substring was read.
+            if (Number.isInteger(temp) === true)
+            {
+                return parseInt(semester.substring(2, 6)) * 10 + temp;
+            }
         }
         
         // Return invalid semesters as 0.
         return 0;
     }
 
-    useEffect(() => {
+    function CheckPrerequisitesMet(entry_id, proposed_semester)
+    {
+        var remaining_prerequisites = [];
 
-        var temp;
-
-        // Loop through all courses, and generate numeric representations of semesters.
-        for (let i = 0; i < courses.length; i++)
+        //Populates remaining_prerequisites as a deep copy of the prerequisites.
+        for (let i = 0; i < courses[entry_id]["prerequisites"].length; i++)
         {
-            temp = courses[i].semester;
-
-            // Verifies that a valid semester value was retrieved.
-            if (temp)
-            {
-                courses[i]["semester_val"] = SemesterToInt(temp);
-
-                // console.log(courses[i]["semester_val"]);
-            }
+            remaining_prerequisites.push(courses[entry_id]["prerequisites"][i]);
         }
 
-    }, [courses]);
+        var proposed_semester_val = SemesterToInt(proposed_semester);
 
-    function CheckPrerequisitesMet(entry_id, proposed_semester_val)
-    {
-        var remaining_prerequisites = courses[entry_id]["prerequisites"];
-        
-        console.log("Prerequisites Check Starting, checking for " + remaining_prerequisites.length + " prerequisites.");
+        //console.log("Prerequisites Check Starting, checking for " + remaining_prerequisites.length + " prerequisites.");
 
         if (remaining_prerequisites.length > 0)
         {
             // Loop through all courses, checking whether they satisfy a prerequisite requirement.
             for (let i = 0; i < courses.length; i++)
             {
-                console.log((courses[i]["semester_val"] < proposed_semester_val) + ", " + (remaining_prerequisites.includes(courses[i]["course_code"]) === true));
-
-                // Checks if it's found in the prerequisites list and if the specific course is included in an earlier semester.
-                if ((courses[i]["semester_val"] < proposed_semester_val) && (remaining_prerequisites.includes(courses[i]["course_code"]) === true))
+                //Validates that the semester is non-null.
+                if (courses[i] && courses[i].semester)
                 {
-                    // Removes the element from the list of remaining prerequisites.
-                    remaining_prerequisites.splice(remaining_prerequisites.findIndex(element => element.course_code === courses[i]["course_code"]), 1);
-                    
-                    if (remaining_prerequisites === 0)
+                    /*
+                    console.log("New Iteration for " + courses[i]["course_code"]);
+                    console.log((SemesterToInt(courses[i].semester) < proposed_semester_val) + ", " + (remaining_prerequisites.includes(courses[i]["course_code"]) === true));
+                    for (let v = 0; v < remaining_prerequisites.length; v++)
                     {
-                        return true;
+                        console.log(remaining_prerequisites[v]);
+                    }
+                    */
+
+                    // Checks if it's found in the prerequisites list and if the specific course is included in an earlier semester.
+                    if ((SemesterToInt(courses[i].semester) < proposed_semester_val) && (remaining_prerequisites.includes(courses[i]["course_code"]) === true))
+                    {
+                        // Removes the element from the list of remaining prerequisites.
+                        remaining_prerequisites.splice(remaining_prerequisites.findIndex((element) => element === courses[i]["course_code"]), 1);
+                        
+                        if (remaining_prerequisites.length === 0)
+                        {
+                            return true;
+                        }
                     }
                 }
             }
@@ -172,6 +185,31 @@ function Planner()
         return true;
     };
 
+    function handleDragStart(event)
+    {
+        SetDragging(event.active);
+    };
+
+    function handleDragEnd(event)
+    {
+        const {over} = event;
+
+        // Checks if the object was dropped over a container.
+        if (over)
+        {
+            // Only allow the object to be dropped into the semester if its prerequisites were satisfied.
+            if (CheckPrerequisitesMet(dragging.id, over.id) === true)
+            {
+                SetCourses([...courses, courses[dragging.id].semester = over.id]);
+            }
+        }
+        // Handles the case for dropping the object over nothing.
+        else
+        {
+            SetCourses([...courses, courses[dragging.id].semester = null]);
+        }
+    };
+
     return (
         <div className="page-container">
             <div className="medium-text text-align-left">
@@ -180,23 +218,35 @@ function Planner()
 
             <div style={{height: "12px"}} />
 
-            <div className="row-container" style={{gap: "12px"}}>
-                {
-                    semesters.map((semester) => (
-                        <div key={semester} onDragEnter={(e) => onDragEnter(e, semester)}>
-                            <SemesterGroup semester_name={semester} key={semester} onDragEnter={(e) => onDragEnter(e, semester)}>
-                                {
-                                    courses.filter((item) => item.semester === semester).map((course) => (
-                                        <div key={course.id} id={course.id} draggable onDragStart={(e) => handleDragStart(e, course.id)}>
-                                            <CourseNode course_code={course.course_code} course_desc={course.course_desc} />
-                                        </div>
-                                    ))
-                                }
-                            </SemesterGroup>
-                        </div>
-                    ))
-                }
-            </div>
+            <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+
+                <div className="row-container" style={{gap: "12px"}}>
+                    <div className="course-node-group-container col-container" style={{gap: "12px"}}>
+                        {
+                            courses.filter((course) => (course && course.semester === null)).map((course) => (
+                                <div key={course.id}>
+                                    <CourseNode key={course.id} course_id={course.id} course_code={course.course_code} course_desc={course.course_desc} />
+                                </div>
+                            ))
+                        }
+                    </div>
+                    {
+                        semesters.map((semester) => (
+                            <div key={semester}>
+                                <SemesterGroup key={semester} semester_name={semester}>
+                                    {
+                                        courses.filter((course) => (course && course.semester === semester)).map((course) => (
+                                            <div key={course.id}>
+                                                <CourseNode key={course.id} course_id={course.id} course_code={course.course_code} course_desc={course.course_desc} />
+                                            </div>
+                                        ))
+                                    }
+                                </SemesterGroup>
+                            </div>
+                        ))
+                    }
+                </div>
+            </DndContext>
         </div>
     )
 }
