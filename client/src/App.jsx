ef80c304cace6fc283c148375a5e356e3256f395
Haley <HaleyPatel262@gmail.com>
2025-10-29 15:57:18 -0400

    Menu Done + Drag and Drop to and from Menu and Semester Column
    The current Menu is hardcoded or not backed from a backend. This is  for the purpose of a demo. User can drag and drop course and placeholders from Menu to Semester column and from Semester column to Menu.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
-import Planner from "./pages/Planner.jsx";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TransferCoursesPage from "./pages/TransferCoursesPage";
+import Planner from "./pages/Planner";
+import "./styles/Planner.css";
 
 function App() {
-    return (
-        <div>
-            <Planner />
-        </div>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<TransferCoursesPage />} />
+        <Route path="/planner" element={<Planner />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
